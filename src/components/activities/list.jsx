06c33fcbde90aa114f4a743e2020b24efc2f0bf3
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-02-03 22:21:54 -0500

    Initial list implementation

--- src/components/activities/list.jsx
+++ src/components/activities/list.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
+import { Avatar, Divider, List, ListItem, ListItemText, ListItemAvatar, Typography } from '@mui/material';
 import { ContactPhone } from '@mui/icons-material';
 
 
@@ -19,20 +15,38 @@ export default function Activities() {
 		const activitesService = fetchActivities();
 
 		activitesService.then((result) => {
+			console.log("#DEBUG ..", result);
 			setActivities(result);
 		});
 	}, [])
 
 	return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ContactPhone />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
+    <List sx={{ width: '100%', maxWidth: 360, maxHeight: 400, overflow: 'auto', bgcolor: 'background.paper' }}>
+			{ activities.map((activity, index) => (
+				<React.Fragment>
+				<ListItem key={`activity_${index}`} alignItems='flex-start'>
+					<ListItemAvatar>
+						<Avatar>
+							<ContactPhone />
+						</Avatar>
+					</ListItemAvatar>
+					<ListItemText primary={activity.call_type} secondary={
+						<React.Fragment>
+						<Typography
+							sx={{ display: 'inline' }}
+							component="span"
+							variant="body2"
+							color="text.primary"
+						>
+							Ali Connors
+						</Typography>
+						{" — I'll be in your neighborhood doing errands this…"}
+					</React.Fragment>
+					} />
+				</ListItem>
+				<Divider variant="inset" component="li" />
+				</React.Fragment>
+			))}
     </List>
   );
 }
