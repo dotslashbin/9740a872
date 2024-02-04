854dbaebcf40186518d27b7a15a41406ae0de28c
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-02-04 14:44:33 -0500

    Refactored list to import a new "listItem" individual component

--- src/components/activities/list.jsx
+++ src/components/activities/list.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-import { Avatar, Divider, List, ListItem, ListItemText, ListItemAvatar, Typography } from '@mui/material';
-import { CALL_TYPES_COLORS } from '../../config';
-import { ContactPhone } from '@mui/icons-material';
-import moment from 'moment';
-
+import { Divider, List } from '@mui/material';
 
 // Services
-import { fetchActivities } from '../../services/activities/activitiesReader';
+import { fetchActivities } from '../../services';
+import Activity from './listItem';
 
 export default function Activities() {
 
 	const [ activities, setActivities ] = useState([]); 
+
+	const updateList = (id, newStatus) => {
+		setActivities(activities.map(item => item.id === id
+			? { ...item, 'is_archived': newStatus }
+			:item
+		));
+
+	};
 
 	useEffect(() => {
 		const activitesService = fetchActivities();
@@ -19,36 +24,20 @@ export default function Activities() {
 		activitesService.then((result) => {
 			setActivities(result);
 		});
-	}, [])
+	}, []);
+
+	useEffect(() => {
+		console.log('#DEBUG .. updated activities');
+	}, [activities]);
 
 	return (
-    <List sx={{ width: '100%', maxWidth: 360, maxHeight: 400, overflow: 'auto', bgcolor: 'background.paper' }}>
-			{ activities.map((activity, index) => (
+		<List sx={{ width: '100%', maxWidth: 360, maxHeight: 400, overflow: 'auto', bgcolor: 'background.paper' }}>
+			{ activities.map((call, index) => !call.is_archived ? (
 				<React.Fragment key={`activity_${index}`}>
-				<ListItem alignItems='flex-start'>
-					<ListItemAvatar>
-						<Avatar>
-							<ContactPhone style={{color: CALL_TYPES_COLORS[activity.call_type]}}/>
-						</Avatar>
-					</ListItemAvatar>
-					<ListItemText primary={activity.call_type} secondary={
-						<React.Fragment>
-						<Typography
-							sx={{ display: 'inline' }}
-							component="span"
-							variant="caption"
-							color="text.primary"
-						>
-							From: {activity.from}
-							<br />
-							{moment(activity.created_at).format('MMMM Do YYYY, h:mm:ss')}
-						</Typography>
-					</React.Fragment>
-					} />
-				</ListItem>
-				<Divider variant="inset" component="li" />
+					<Activity data={call} updateList={updateList}/>
+					<Divider variant="inset" component="li" />
 				</React.Fragment>
-			))}
-    </List>
-  );
+			): null )}
+		</List>
+	);
 }
